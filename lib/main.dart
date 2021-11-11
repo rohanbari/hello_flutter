0ec5173fc722b6d80c5e0fee087ea1219256535b
rohanbari <rohanbari4@gmail.com>
2021-11-11 18:27:58 +0530

    MaterialApp application from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,28 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
+// MyApp class adapting and extending StatelessWidget widget.
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Returning a MaterialApp widget that contains a scaffold.
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hello Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        body: const Text("Goodbye, world!"),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // On press, print the following debug line.
+            print("Hello");
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
       ),
-      home: const MyHomePage(title: 'Increment and Decrement'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      // Limiting the decrement up to zero.
-      if (_counter > 0) {
-        _counter--;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            FloatingActionButton(
-              onPressed: _incrementCounter,
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            ),
-            FloatingActionButton(
-              onPressed: _decrementCounter,
-              tooltip: 'Decrement',
-              child: const Icon(Icons.remove),
-            ),
-          ],
-        ));
   }
 }
